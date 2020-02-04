4f9b43fb600b6aeec0f42e19ad0baa694bd3128c
roxanahefferon <roxana.hefferon@gmail.com>
2020-02-04 00:48:22 +0000

    fixed divs for cards, visibility and some styling

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-
 document.getElementById("play").addEventListener("click", function(){
-    alert("im sick of this");
+    alert("im sick of this blah blah =)");
 });
+
+
+// timer
+
+function countDown(i, callback) {
+    let timer = setInterval(function(){
+        document.getElementById("time-left").innerHTML = "Time: " + i;
+        i-- || (clearInterval(timer), callback());
+    }, 1000);
+}
