2a793a8068763675a89f8dd993e3bffdd2cd433a
roxanahefferon <roxana.hefferon@gmail.com>
2020-02-07 10:53:53 +0000

    shuffling

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ let firstCard, secondCard;
 
 function flipCard() {
   if (lockBoard) return;
+  if (this === firstCard) return;
   this.classList.add('flip');
   if (!hasFlippedCard) {
     hasFlippedCard = true;
@@ -15,7 +16,6 @@ function flipCard() {
    }
 
    secondCard = this;
-   hasFlippedCard = false;
 
    checkForMatch();
  }
@@ -34,16 +34,29 @@ function flipCard() {
    secondCard.removeEventListener('click', flipCard);
  }
 
+ resetBoard();
+
  function unflipCards() {
 
     lockBoard = true;
     setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-    lockBoard = false;
+    resetBoard();
    }, 1500);
  }
 
+ function resetBoard() {
+   [hasFlippedCard, lockBoard] = [false, false];
+   [firstCard, secondCard] = [null, null];
+ }
+
+ (function shuffle() {
+   cards.forEach(card => {
+     let ramdomPos = Math.floor(Math.random() * 16);
+     card.style.order = ramdomPos;
+   });
+ })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
