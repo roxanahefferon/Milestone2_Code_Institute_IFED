973633c7c661e8e10c1428f6bba100bfdb60bad6
roxanahefferon <roxana.hefferon@gmail.com>
2020-02-07 17:22:04 +0000

    work on quiz array

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,3 +2,42 @@ const quizQuestion = document.getElementById('quizQuestion');
 const quizResult = document.getElementById('quizResult');
 const quizButton = document.getElementById('quizButton');
 
+const myQuestions = [
+  {
+    question: "Which Harry Potter word is now in the Oxford English Dictionary?",
+    answers: {
+      a: "Hogwarts",
+      b: "Muggle",
+      c: "Voldemort"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Who were Harry's parents?",
+    answers: {
+      a: "Henry and Maggie Potter",
+      b: "William and Elizabeth Potter",
+      c: "James and Lily Potter"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who are the muggle aunt and uncle that Harry must live with every summer?",
+    answers: {
+      a: "Vernon and Petunia Dursley",
+      b: "Ned and Camilla Diddly",
+      c: "Ralph and Magnolia Dudley",
+    },
+    correctAnswer: "a"
+  }
+];
+
+function buildQuiz(){}
+
+function showResults(){}
+
+// display quiz right away
+buildQuiz();
+
+// on submit, show results
+submitButton.addEventListener('click', showResults);
